1c9038fe7a65001bc33399b5f8a1255bfcf0102a
Woooor-VKI <114291794+Woooor-VKI@users.noreply.github.com>
2023-11-25 10:22:13 +0300

    Добавьте файлы проекта.

---
+++ Lab4v2/Program.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using Library;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(); // Создание объекта-браузера
+            driver.Navigate().GoToUrl("https://pikabu.ru/best");
+            var articles = driver.FindElements(By.ClassName("story"));
+
+            Console.WriteLine(articles.Count);
+
+            foreach (var article in articles)
+            {
+                var message = article.FindElement(By.ClassName("story__title")).Text;
+                var name = article.GetAttribute("data-author-name");
+                var id = article.GetAttribute("data-story-id");
+
+                Console.WriteLine($"{id} - {name} - {message}");
+
+                // ApplicationData bd = new ApplicationData();
+                // bd.AddRecord(name,message);
+            }
+            driver.Quit();
+        }
+    }
+}
---
+++ Library/ApplicationData.cs
@@ -0,0 +1,58 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class ApplicationData
+    {
+        public static Db_Lab4Entities1 db = new Db_Lab4Entities1();
+
+        public recordNode GetById(int id)
+        {
+            return db.recordNode.FirstOrDefault(x => x.Id == id);
+        }
+
+        public recordNode GetByName(string name)
+        {
+            return db.recordNode.FirstOrDefault(x => x.Name == name);
+        }
+
+        public void AddRecord(string name, string messege)
+        {
+            recordNode recordNode1 = db.recordNode.FirstOrDefault(x => x.Name == name && x.Message == messege);
+            if (recordNode1 == null)
+            {
+                recordNode recordNode = new recordNode();
+                recordNode.Name = name;
+                recordNode.Message = messege;
+                db.recordNode.Add(recordNode);
+                db.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Такой пользователь с такой записью уже есть!");
+            }
+        }
+
+        public void UpdateRecord(int id, string messageUpdate)
+        {
+            recordNode recordNode = db.recordNode.FirstOrDefault(x => x.Id == id);
+            recordNode.Message = messageUpdate;
+
+            db.recordNode.AddOrUpdate(recordNode);
+            db.SaveChanges();
+        }
+
+        public void RemoveRecord(int id)
+        {
+            recordNode recordNode = db.recordNode.FirstOrDefault(x => x.Id == id);
+            db.recordNode.Remove(recordNode);
+            db.SaveChanges();
+        }
+    }
+}
